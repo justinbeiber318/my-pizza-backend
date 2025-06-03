d96e4f4310cc1c342bf50c832b00493d906db837
justinbeiber318 <huygia31825@gmail.com>
2025-06-03 20:28:22 +0700

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -1,5 +1,5 @@
 <?php
-$servername = "sql12.freesqldatabase.com	";       // Máy chủ MySQL
+$servername = "sql12.freesqldatabase.com";       // Máy chủ MySQL
 $username = "sql12782808";              // Tên người dùng (root hoặc tài khoản của bạn)
 $password = "4pqTr9sp7M";                  // Mật khẩu của tài khoản MySQL (trong trường hợp này là trống cho "root")
 $dbname = "sql12782808";              // Tên cơ sở dữ liệu
