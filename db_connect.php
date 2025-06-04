b3c2214f13e0a2436deb34dacbed371911316963
justinbeiber318 <huygia31825@gmail.com>
2025-06-04 10:18:58 +0000

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "sql12.freesqldatabase.com";       // Máy chủ MySQL
-$username = "sql12782808";              // Tên người dùng (root hoặc tài khoản của bạn)
-$password = "4pqTr9sp7M";                  // Mật khẩu của tài khoản MySQL (trong trường hợp này là trống cho "root")
-$dbname = "sql12782808";              // Tên cơ sở dữ liệu
+$servername = "localhost";       // Máy chủ MySQL
+$username = "omkxjnto_pizzas";              // Tên người dùng (root hoặc tài khoản của bạn)
+$password = "AaHSp5CCn6u5W7Mr8Pzr";                  // Mật khẩu của tài khoản MySQL (trong trường hợp này là trống cho "root")
+$dbname = "omkxjnto_pizzas";              // Tên cơ sở dữ liệu
 
 // Tạo kết nối
 $conn = new mysqli($servername, $username, $password, $dbname);
